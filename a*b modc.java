435bd045eb12f253fe74c71adef50158190e7e61
janani1998 <41136718+janani1998@users.noreply.github.com>
2018-08-08 12:59:48 +0530

    Create a*b modc.java

---
+++ a*b modc.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int a=2,b=8,c=6,d=0,rem=0;
+	  d=a*b;
+	  rem=d%c;
+	  System.out.print(rem);
+	}
+}
